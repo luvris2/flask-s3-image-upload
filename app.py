8d9e77d01eebb7c8b0b4a30ac2ce8d83069cd183
luvris2 <luvris2@naver.com>
2022-06-23 17:09:48 +0900

    add

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+# API 서버를 구축하기 위한 기본 구조
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config) # 만들었던 Config.py의 Config 클래스 호출
+
+# JWT 토큰 생성
+jwt = JWTManager(app)
+
+# 로그아웃 된 토큰이 들어있는 set을 jwt에게 알림
+@jwt.token_in_blocklist_loader
+def check_it_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in jwt_blocklist
+
+# restfulAPI 생성
+api = Api(app)
+
+# 경로와 리소스(api코드) 연결
+#api.add_resource(MovieListResource, '/movie_list')
+
+if __name__ == '__main__' :
+    app.run()
